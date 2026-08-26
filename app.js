cc506c2c09f504a2280fd3b6d9081b5c94677705
ladashum <ladanazarenko0202@gmail.com>
2026-08-26 16:03:57 +0300

    Delete app.js

--- app.js
+++
@@ -1,3 +0,0 @@
-const LABELS=['1','2','3','4','5'];
-const TRAITS=['lead','nurture','gentleman','flow','stage','quiet','craft','heart','care','philo','verbal','cool','drive'];
-console.log('svt test loaded');
