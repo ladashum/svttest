a47547f310064b50d922c5a6d3a09444e0861215
ladashum <ladanazarenko0202@gmail.com>
2026-08-26 16:00:11 +0300

    Add app.js with simplified questions

---
+++ app.js
@@ -0,0 +1,3 @@
+const LABELS=['1','2','3','4','5'];
+const TRAITS=['lead','nurture','gentleman','flow','stage','quiet','craft','heart','care','philo','verbal','cool','drive'];
+console.log('svt test loaded');
